16532a7276a743d7b09bd0b41c8f45e39d26f201
Caleb Durojaiye <dco2.caleb@gmail.com>
2025-12-23 14:26:33 +0100

    bring in the circular text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Silkscreen } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +11,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const silkscreen = Silkscreen({
+  variable: "--font-silkscreen",
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${silkscreen.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,65 @@
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div>
+      <main>
+        <div className="flex flex-row items-center justify-between pt-4 pb-4 bg-black text-white">
+          <div className="ml-4">
+            <div className="font-header text-xl">
+              LOCALBOAT:<br/>
+              :STUDIO
+            </div>
+          </div>
+          <div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/boat-desktop.png"
+              alt="header boat illustration"
+              width={59.8}
+              height={33.2}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <div className="mr-4">
+            ◶ ✪ ◍
+          </div>
         </div>
+        {/* <div className="h-75 flex flex-row items-center justify-between"> */}
+          <div className="ml-4 absolute">
+            <div className="inline-block bg-amber-100 h-[176.8px] w-[227.73px]">
+              <span className="text-blue-400 font-header">WE’RE</span><br/>
+              <span>PERFECTING THE CRAFT OF</span><br/>
+              <span className="text-4xl">BRANDING</span><br/>
+              <span className="bg-black text-white">PRODUCTS AND </span><br/>
+              <span className="bg-red-400 text-black">WEBSITES</span><br/>
+              <span>FOR <span className="ml-26"></span><span className="text-blue-400 text-4xl font-header">YOU</span></span><br/>
+            </div>
+
+            {/* <div className="">
+              Circle Text
+            </div> */}
+          </div>
+          <div className="flex justify-center items-center h-150 w-full border border-black border-solid">
+            <div className="inline-block bg-amber-100 h-[176.8px] w-[227.73px]">
+              {/* <span className="text-blue-400 font-header">WE’RE</span><br/>
+              <span>PERFECTING THE CRAFT OF</span><br/>
+              <span className="text-4xl">BRANDING</span><br/>
+              <span className="bg-black text-white">PRODUCTS AND </span><br/>
+              <span className="bg-red-400 text-black">WEBSITES</span><br/>
+              <span>FOR <span className="ml-26"></span><span className="text-blue-400 text-4xl font-header">YOU</span></span><br/> */}
+            </div>
+          </div>
+          {/* <div className="rotate-90 origin-top-left">
+            localboat:3000/your/offline/is/where/important/work/can/happen
+          </div> */}
+          {/* <div className="relative">
+            <span className="absolute left-0 bottom-0 rotate-90 origin-bottom-left">
+                SECTION TITLE
+            </span>
+          </div> */}
+        {/* </div> */}
       </main>
+
     </div>
   );
 }
